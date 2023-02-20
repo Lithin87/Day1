778cadca61ef790351226b820e6deb03151dd231
Lithin Thomas <72386223+Lithin87@users.noreply.github.com>
2023-02-20 10:57:23 +0000

    did a rerun

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import  fs from 'fs'
-import  {default as abc , name , name1}   from './input.js'
+import  fsp from 'fs/promises'
+import  {default as abc}   from './input.js'
 
 abc("./data")
-console.log("data is coming");
+console.log("Main file running");
 
-console.log("values"+ name + name1);
 
 
 // let data = {
@@ -13,32 +12,9 @@ console.log("values"+ name + name1);
 // }
 
 
-// console.log("fff");
-// console.log(data);
-
-
 // const {as : { [3] : first} } = data;
 // console.log(first);
 
-// const {sdsd } = data.df
-// console.log(sdsd);
-
-
-// start_fn(data)
-// function start_fn({as,df,df1})
-// {
-//     console.log(as);
-//     console.log(df);
-
-// }
-
-// function start_fn(bloop)
-// {
-//     let { as ,df1} = bloop
-//     console.log(as);
-//     console.log(df1);
-
-// }
 
 // function SuperHero(name, strength,speed)
 // {
--- input.js
+++ input.js
@@ -1,18 +1,27 @@
-import { readdir } from 'fs/promises';
+import fsp from 'fs/promises';
 
 
-export default async function abc(data)
-{
-    const files = await readdir(data);
-    for (const file of files)
-      console.log(file)
+export default async function abc(data) {
+  console.log("STARTED")
+
+  const files = await fsp.readdir(data);
+  let m = new Map();
+
+
+  for (const file of files) {
+    const fileRead = await fsp.readFile('data/' + file, { encoding: "utf-8" })
+    m.set(file,fileRead.match(/gotYa/g)?.length)
+  }
+  
+  const cv = Array.from(m).sort(([k,v],[k1,v1]) =>  v-v1 )
+  let m1 = new Map(cv);
+  console.log(m1)
+
+  
+
 }
 
-let name = "Lithin"
-let name1 = "Lithin1"
 
-
-export { name  ,name1 };
 // export { variable1 as name1, variable2 as name2, /* …, */ nameN };
 // export { variable1 as "string name" };
 // export { name1 as default /*, … */ };
@@ -20,15 +29,15 @@ export { name  ,name1 };
 
 
 export async function readThenClose() {
-    let filehandle = null;
+  let filehandle = null;
 
-        // Using the filehandle method 
-        // filehandle = await fsPromises.open('input.txt', 'r+');
+  // Using the filehandle method 
+  // filehandle = await fsPromises.open('input.txt', 'r+');
 
-        var data =  await fsPromises.readFile('input.txt',"utf8");
-        console.log(data);
-        console.log(Array.from(data));
-        console.log("File Closed!");
+  var data = await fsPromises.readFile('input.txt', "utf8");
+  console.log(data);
+  console.log(Array.from(data));
+  console.log("File Closed!");
 }
 
 // readThenClose().catch((error) => {   console.log("Error", error)});
@@ -37,5 +46,3 @@ export async function readThenClose() {
 // module.exports = function (msg) { 
 //     console.log(msg);
 // };
-
-export * as fs1 from "fs"
