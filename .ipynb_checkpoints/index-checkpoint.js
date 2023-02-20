eb6a03f0d2a1f216eb8fcf3a2885efc00dd159ae
Lithin Thomas <72386223+Lithin87@users.noreply.github.com>
2023-02-20 10:58:12 +0000

    did a rerun1

---
+++ .ipynb_checkpoints/index-checkpoint.js
@@ -0,0 +1,54 @@
+import  fs from 'fs'
+import  {default as abc , name , name1}   from './input.js'
+
+abc("./data")
+console.log("data is coming");
+
+console.log("values"+ name + name1);
+
+
+// let data = {
+//     as : [3,2,1,4],
+//     df : {"sdsd": "dfgdf"}
+// }
+
+
+// console.log("fff");
+// console.log(data);
+
+
+// const {as : { [3] : first} } = data;
+// console.log(first);
+
+// const {sdsd } = data.df
+// console.log(sdsd);
+
+
+// start_fn(data)
+// function start_fn({as,df,df1})
+// {
+//     console.log(as);
+//     console.log(df);
+
+// }
+
+// function start_fn(bloop)
+// {
+//     let { as ,df1} = bloop
+//     console.log(as);
+//     console.log(df1);
+
+// }
+
+// function SuperHero(name, strength,speed)
+// {
+//     this.name = name,
+//     this.strength = strength,
+//     this.speed = speed
+// }
+
+
+// let sd = new SuperHero("lithin",23,345);
+// console.log(sd)
+
+
